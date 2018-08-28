b62836055dc8fc3433d017c11cdc878c76316684
Isabelle Delmas <isabelle.lenertz@gmail.com>
2018-08-28 15:42:53 -0700

    initial commit

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    public GameObject laserPrefab;
+    private GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+        laserTransform.LookAt(hitPoint);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y,
+            hit.distance);
+    }
+}
